d3bb0914233909bd2b34603f40bd5595410a7656
Abdul Rahimi <abdul.rahimi_defra@outlook.com>
2020-02-17 23:06:56 +0000

    add routes for twitter and fb

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,35 @@ var passport = require('passport');
 var router = express.Router();
 
 
-router.get('/google/callback/', passport.authenticate('google', { 
-        successRedirect: '/users/',
-        failure: '/error/'
-}));
+router.route('/google/callback')
+        .get(passport.authenticate('google', {
+                successRedirect: '/users/',
+                failure: '/error/'
+        }));
+
+router.route('/google')
+        .get(passport.authenticate('google', {
+                scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                        'https://www.googleapis.com/auth/userinfo.email']
+        }))
+
+router.route('/twitter/callback')
+        .get(passport.authenticate('twitter', {
+                successRedirect: '/users/',
+                failure: '/error/'
+        }));
+
+router.route('/twitter')
+        .get(passport.authenticate('twitter'))
 
 
-router.get('/google', passport.authenticate('google', {
-        scope: ['https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email']
-}))
-
+router.route('/facebook')
+        .get(passport.authenticate('facebook', {
+                scope: ['email']
+        }));
+router.route('/facebook/callback')
+        .get(passport.authenticate('facebook', {
+                successRedirect: '/users',
+                failureRedirect: '/error'
+        }));
 module.exports = router;
-
